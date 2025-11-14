3d9779a332abe079bc56ac20d6671566e3152d97
Gobbercito <Felugobber@gmail.com>
2025-11-14 15:15:02 -0300

    uRLGENERATOR feat

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use illuminate\rauting\urlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,8 +18,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url): void
     {
-        //
+        if (app()->environment() !== 'production') {
+            $this->configureNumberFormatting();
+            $url->forceScheme('https');
+        }
     }
 }
